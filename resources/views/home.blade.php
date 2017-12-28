8ad2d3e6d4c8cf6698352ed3d53ca4f3bf2e359f
smartmaxdev <smartmaxdev@gmail.com>
2017-12-28 09:33:24 +0800

    add carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,30 +3,35 @@
 @section('content')
 	<!-- Carousel Slider -->
 	<div id="carouselLogo" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
 	    <ol class="carousel-indicators">
 	        <li data-target="#carouselLogo" data-slide-to="0" class="active"></li>
 	        <li data-target="#carouselLogo" data-slide-to="1"></li>
 	        <li data-target="#carouselLogo" data-slide-to="2"></li>
 	    </ol>
-	    <div class="carousel-inner" role="listbox">
-	        <div class="carousel-item active">
-	            <img class="d-block img-fluid" src="http://www.facenet.org/wp-content/themes/executive/assets/images/header.jpg" alt="First slide">
+
+	    <!-- Wrapper for slides -->
+	    <div class="carousel-inner" >
+	        <div class="item active">
+	            <img src="/img/carousel1.jpg" alt="First slide">
 	        </div>
-	        <div class="carousel-item">
-	            <img class="d-block img-fluid" src="http://www.facenet.org/wp-content/themes/executive/assets/images/header.jpg" alt="First slide">
+	        <div class="item">
+	            <img src="/img/carousel2.jpg" alt="First slide">
 	        </div>
-	        <div class="carousel-item">
-	            <img class="d-block img-fluid" src="http://www.facenet.org/wp-content/themes/executive/assets/images/header.jpg" alt="First slide">
+	        <div class="item">
+	            <img src="/img/carousel3.jpg" alt="First slide">
 	        </div>
 	    </div>
-	    <a class="carousel-control-prev" href="#carouselLogo" role="button" data-slide="prev">
-	        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-	        <span class="sr-only">Previous</span>
-	    </a>
-	    <a class="carousel-control-next" href="#carouselLogo" role="button" data-slide="next">
-	        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-	        <span class="sr-only">Next</span>
-	    </a>
+
+	    <!-- Left and right controls -->
+		 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		    <span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		    <span class="sr-only">Next</span>
+		</a>
 	</div>
 	<!-- Carousel Slider -->
 
