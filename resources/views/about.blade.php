99a7a64656e8e74451cda155408885b97728afcc
smartmaxdev <smartmaxdev@gmail.com>
2018-01-29 23:32:32 +0800

    update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,8 @@
 
 @section('content')
 	<h1>About</h1>
-	<p>This is about This is test my max this th sample content</p>
+	<p>Over 5 years of mobile & web development experience on the iOS/Android/Web platform. 
+With strong expertise in Swift/Objective-C, Android SDK and PHP. 
+I have strong experience in 3rd party services such as AWS, Azure, Layer, Google App Engine, Stripe, and Firebase. 
+I have created, and improved, dozens of successful applications for clients in areas of dating, education, entertainment, self-help, fitness, and commerce. I have maintained 100% customer satisfaction and repeat business by truly caring about my client's products, budget, and communication needs. I am passionate about mobile & web development and that always comes across in the quality of my work.I welcome discussing your projects and what we can do together to ensure success!</p>
 @endsection
